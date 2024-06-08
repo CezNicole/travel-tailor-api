ab42148067f61ea1d4a5af487940ba860ece3366
Cez Nicole Comia <cezcomia@gmail.com>
2024-06-08 06:10:28 -0400

    modify province controller to display random image per province

--- controllers/provinces-controller.js
+++ controllers/provinces-controller.js
@@ -6,7 +6,24 @@ const getProvinces = async (_req, res) => {
             .distinct()
             .select(['province_territory']);
 
-        res.status(200).json(data.map(row => row.province_territory));
+        const provincesWithRandomImage = [];
+        for (let i=0; i<data.length; i++) {
+            const province = data[i];
+            const images = await knex('attractions')
+                .select('image_link')
+                .where('province_territory', province.province_territory);
+
+            const randomImage = images[Math.floor(Math.random() * images.length)];
+
+            provincesWithRandomImage.push({
+                province_territory: province.province_territory,
+                image_link: randomImage ? randomImage.image_link : null
+            });
+        }
+
+        // res.status(200).json(data.map(row => row.province_territory));
+        res.status(200).json(provincesWithRandomImage);
+
     } catch (error) {
         res.status(500).send(`Error retrieving Canadian provinces/territories: ${error}`);
     }
